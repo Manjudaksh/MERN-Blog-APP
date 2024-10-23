5a5f875285a926671789c26fca2cee8a4a56d192
Manjudaksh <manjudaksh1400@gmail.com>
2024-10-23 06:21:57 +0530

    complete dark mode functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,100 +1,107 @@
-// import { application } from 'express';
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useDispatch, useSelector } from 'react-redux';
-import { signInFailure, signInStart, signInSuccess } from '../redux/User/userSlice';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
-export default function SignUp() {
-
-  const [formData, setFormData] = useState({});
-  const { loading, error: errorMessage } = useSelector(state => state.user);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const changeHandler = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value.trim() });
-  };
-  console.log(formData);
-
-  const submitHandler = async(e) => {
-    e.preventDefault();
-    if( !formData.email || !formData.password){
-      return dispatch(signInFailure('Please fill all the fields'));
+export default function SignIn() {
+    // const [errorMessage, setErrorMessage] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({});
+    const {loading, error: errorMessage} = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.id]: e.target.value.trim()});
     }
-    try{
-      dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      if(data.success === false){
-        dispatch(signInFailure(data.message));
-      }
-      if(res.ok){
-        dispatch(signInSuccess(data));
-        navigate('/');
-      }
-    }catch (error){
-      dispatch(signInFailure(error.message));
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(!formData.email || !formData.password){
+            return dispatch(signInFailure('Please Fill out all fields'));
+        }
+        try{
+            dispatch(signInStart());
+            const res = await fetch('/api/auth/signin', {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if(data.success === false){
+                dispatch(signInFailure(data.message));
+            }
+            if(res.ok){
+                dispatch(signInSuccess(data));
+                navigate('/')
+            }
+        }catch(error) {
+            dispatch(signInFailure(error.message));
+        }
     }
-  };
 
-  return <div className='min-h-screen mt-20'>
-    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-      {/* left */}
-      <div className='flex-1'>
-        <Link to="/" className='font-bold dark:text-white text-4xl'>
-          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
-          via-purple-500 to-pink-500 rounded-lg text-white'>Manju's</span>
-          Blog
-        </Link>
-        <p className='text-sm mt-5'>
-          Welcome to Manju's blog. You can sign in with email and password
-          or with Google
-        </p>
-      </div>
-      {/* right */}
-      <div className='flex-1'>
-        <form className='flex flex-col gap-4' onSubmit={submitHandler}>
-          <div>
-            <Label value='Your email'></Label>
-            <TextInput type='email' placeholder='Email' id='email' onChange={changeHandler}/>
-          </div>
-          <div>
-            <Label value='Your password'></Label>
-            <TextInput type='password' placeholder='********' id='password' onChange={changeHandler}/>  
-          </div>
-          <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
-            {
-              loading ? (
-                <>
-                  <Spinner size='sm'/>
-                  <span className='pl-3'>Loading...</span>
-                </>  
-              ) : (
-                'Sign In'
-              )}
-          </Button>
-          <OAuth/>
-        </form>
-        <div className='flex gap-2 text-sm mt-5'>
-          <span>Don't have an account?</span>
-            <Link to='/sign-up' className='text-blue-500'>
-            Sign Up
-            </Link>  
+  return (
+    <div className='min-h-screen mt-20'>
+        <div className='flex flex-col max-w-3xl gap-6 p-3 mx-auto md:items-center md:flex-row'>
+            {/* left */}
+            <div className='flex-1'>
+                <Link to="/" className='text-4xl font-bold dark:text-white'>
+                    <span className='px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Sidd's</span>Blog
+                </Link>
+                <p className='mt-10 text-sm'>
+                    Join our community of writers and readers – Sign in today to share your thoughts and discover new ideas!
+                    <br/>
+                    You can sign in with your username and email or with Google.
+                </p>
+            </div>
+            {/* right */}
+            <div className='flex-1'>
+                <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                    <div>
+                        <Label value='Your email' />
+                        <TextInput 
+                            type='email'
+                            placeholder='name@company.com'
+                            id='email'
+                            onChange={handleChange}
+                        /> 
+                    </div>
+                    <div>
+                        <Label value='Your password' />
+                        <TextInput 
+                            type='password'
+                            placeholder='***********'
+                            id='password'
+                            onChange={handleChange}
+                        /> 
+                    </div>
+                    <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                        {
+                            loading ? (
+                                <>
+                                    <Spinner size='sm' />
+                                    <span className='pl-3'>Loading...</span>
+                                </>
+                            ) : 'Sign In'
+                        }
+                    </Button>
+                    <OAuth />
+                </form>
+                <div className='flex gap-2 mt-5 text-sm'>
+                    <span>Haven't an account?</span>
+                    <Link to='/signup' className='text-blue-500'>
+                        Sign Up
+                    </Link>
+                </div>
+                {
+                    errorMessage && (
+                        <Alert className='mt-5' color='failure'>
+                            {errorMessage}
+                        </Alert>
+                    )
+                }
+            </div>
         </div>
-        {
-          errorMessage && (
-            <Alert className='my-5' color='failure'>
-              {errorMessage}
-            </Alert>
-        )}
-      </div>
-    </div>  
-  </div>
+    </div>
+  )
 }
-
